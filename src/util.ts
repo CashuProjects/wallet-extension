9621fadc0ca49a6e6e8527306e4b9bacc502f260
rxbryan <rxbryn@gmail.com>
2023-10-31 23:22:42 +0100

    fix: lint

--- src/util.ts
+++ src/util.ts
@@ -1,13 +1,13 @@
-import {Token} from '@cashu/cashu-ts'
+import { Token } from '@cashu/cashu-ts';
 
 export function generateRandomString(length) {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor((Date.now() * Math.random()) %  charactersLength));
-  }
-  return result;
+	let result = '';
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	const charactersLength = characters.length;
+	for (let i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor((Date.now() * Math.random()) % charactersLength));
+	}
+	return result;
 }
 
 /**
@@ -21,19 +21,19 @@ export function generateRandomString(length) {
  * @return {*}  {Token}
  */
 export function cleanToken(token: Token): Token {
-  const tokenEntryMap: { [key: string]: TokenEntry } = {};
-  for (const tokenEntry of token.token) {
-    if (!tokenEntry?.proofs?.length || !tokenEntry?.mint) {
-      continue;
-    }
-    if (tokenEntryMap[tokenEntry.mint]) {
-      tokenEntryMap[tokenEntry.mint].proofs.push(...[...tokenEntry.proofs]);
-      continue;
-    }
-    tokenEntryMap[tokenEntry.mint] = { mint: tokenEntry.mint, proofs: [...tokenEntry.proofs] };
-  }
-  return {
-    memo: token?.memo,
-    token: Object.values(tokenEntryMap).map((x) => ({ ...x, proofs: sortProofsById(x.proofs) }))
-  };
+	const tokenEntryMap: { [key: string]: TokenEntry } = {};
+	for (const tokenEntry of token.token) {
+		if (!tokenEntry?.proofs?.length || !tokenEntry?.mint) {
+			continue;
+		}
+		if (tokenEntryMap[tokenEntry.mint]) {
+			tokenEntryMap[tokenEntry.mint].proofs.push(...[...tokenEntry.proofs]);
+			continue;
+		}
+		tokenEntryMap[tokenEntry.mint] = { mint: tokenEntry.mint, proofs: [...tokenEntry.proofs] };
+	}
+	return {
+		memo: token?.memo,
+		token: Object.values(tokenEntryMap).map(x => ({ ...x, proofs: sortProofsById(x.proofs) }))
+	};
 }
